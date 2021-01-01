c639a47df1883e06504379a579aad232fde29026
Heon_9u <frtt0608@naver.com>
2021-01-01 23:42:37 +0900

    210101 | api요청 클래스 생성 | accuweather에서 openweather 또는 기상청 api로 변경 예정

--- app/src/main/java/com/heon9u/alarm_weather_app/AccuweatherApi.java
+++ app/src/main/java/com/heon9u/alarm_weather_app/AccuweatherApi.java
@@ -1,4 +1,49 @@
 package com.heon9u.alarm_weather_app;
 
-public class AccuweatherApi {
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class AccuweatherApi extends AsyncTask<String, Void, String> {
+    private final String serviceKey = "AZPtVq7G1R652UPuvauPNNoBBXbgxZCz";
+    private final String locationsUrl = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    private final String forecastsUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/";
+    String locationKey = "";
+
+    @Override
+    protected String doInBackground(String... location) {
+        try {
+            return searchLocationKey((String) location[0]);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Access accuweatherApi failed";
+        }
+    }
+
+    public String searchLocationKey(String location) {
+
+        HttpURLConnection conn = null;
+
+        try {
+            String strUrl = locationsUrl + "?apikey=" + serviceKey + "&q=" + location;
+            URL url = new URL(strUrl);
+            conn = (HttpURLConnection) url.openConnection();
+
+            if(conn.getResponseCode() == conn.HTTP_OK) {
+                InputStreamReader isr = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally{
+            conn.disconnect();
+        }
+    }
 }
+
--- app/src/main/java/com/heon9u/alarm_weather_app/FineDustApi.java
+++ app/src/main/java/com/heon9u/alarm_weather_app/FineDustApi.java
@@ -1,4 +1,6 @@
 package com.heon9u.alarm_weather_app;
 
 public class FineDustApi {
+    String serviceKey = "lI0Ngu5M%2BJewHO%2BGcadPtiLVGTc6U4lgwJW8sgKiDLQVZ31N%2BwpXpAiPGoFaJJE0VsqzxAZNruDG8BC3kxkFhQ%3D%3D";
+    String serviceUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/";
 }
