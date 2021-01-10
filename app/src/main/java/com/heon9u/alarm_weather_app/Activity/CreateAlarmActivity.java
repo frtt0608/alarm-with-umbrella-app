646b855faf55537d3a9d166ff97fe8b1f5bb82d6
Heon_9u <frtt0608@naver.com>
2021-01-10 17:31:56 +0900

    210110 | alarm_create 레이아웃 변경

--- app/src/main/java/com/heon9u/alarm_weather_app/Activity/CreateAlarmActivity.java
+++ app/src/main/java/com/heon9u/alarm_weather_app/Activity/CreateAlarmActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,54 +14,78 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.heon9u.alarm_weather_app.Dto.Alarm;
 import com.heon9u.alarm_weather_app.R;
 
-public class CreateAlarmActivity extends AppCompatActivity {
+public class CreateAlarmActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText title;
     TimePicker timePicker;
-    Button create_button, cancel_button;
     int alarmHour, alarmMinute;
+    EditText title;
+    Switch allDaySwitch, allSoundSwitch, basicSoundSwitch, umbSoundSwitch, vibSwitch;
+    TextView basicSound, umbSound;
+    Button[] dayButton = new Button[7];
+    Button create_button, cancel_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm_create);
 
-        timePicker = findViewById(R.id.timePicker);
-        timePicker.setIs24HourView(true);
-        alarmHour = timePicker.getCurrentHour();
-        alarmMinute = timePicker.getCurrentMinute();
-        timePicker.setOnTimeChangedListener(new timeChangedListener());
-        title = findViewById(R.id.title);
+        setTimePicker();
+        setButtonView();
+        setTextView();
 
-        create_button = findViewById(R.id.create_button);
-        cancel_button = findViewById(R.id.cancel_button);
+    }
 
-        create_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.create_button:
                 AppDatabaseHelper appDB = new AppDatabaseHelper(CreateAlarmActivity.this);
 
                 String alarmTitle = title.getText().toString();
                 appDB.createAlarm(alarmHour, alarmMinute, alarmTitle);
 
+            case R.id.cancel_button:
                 backToAlarmListView();
                 finish();
-            }
-        });
 
-        cancel_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                backToAlarmListView();
-                finish();
-            }
-        });
+            case R.id.sun:
+
+        }
     }
 
-    public void backToAlarmListView() {
-        Intent main = new Intent(CreateAlarmActivity.this, MainActivity.class);
-        startActivity(main);
+    public void setTimePicker() {
+        timePicker = findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
+        alarmHour = timePicker.getCurrentHour();
+        alarmMinute = timePicker.getCurrentMinute();
+        timePicker.setOnTimeChangedListener(new timeChangedListener());
     }
+
+    public void setTextView() {
+        title = findViewById(R.id.title);
+        basicSound = findViewById(R.id.basicSound);
+        umbSound = findViewById(R.id.umbSound);
+    }
+
+    public void setButtonView() {
+        allDaySwitch = findViewById(R.id.allDaySwitch);
+        dayButton[0] = findViewById(R.id.sun);
+        dayButton[1] = findViewById(R.id.mon);
+        dayButton[2] = findViewById(R.id.tue);
+        dayButton[3] = findViewById(R.id.wen);
+        dayButton[4] = findViewById(R.id.thu);
+        dayButton[5] = findViewById(R.id.fri);
+        dayButton[6] = findViewById(R.id.sat);
+        allSoundSwitch = findViewById(R.id.allSoundSwitch);
+
+        basicSoundSwitch = findViewById(R.id.basicSoundSwitch);
+        umbSoundSwitch = findViewById(R.id.umbSoundSwitch);
+        vibSwitch = findViewById(R.id.vibSwitch);
+
+        create_button = findViewById(R.id.create_button);
+        cancel_button = findViewById(R.id.cancel_button);
+    }
+
 
     public class timeChangedListener implements TimePicker.OnTimeChangedListener {
 
@@ -68,5 +94,10 @@ public class CreateAlarmActivity extends AppCompatActivity {
             alarmHour = hour;
             alarmMinute = minute;
         }
+    }
+
+    public void backToAlarmListView() {
+        Intent main = new Intent(CreateAlarmActivity.this, MainActivity.class);
+        startActivity(main);
     }
 }
