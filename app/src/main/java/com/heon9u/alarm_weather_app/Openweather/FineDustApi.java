674564781fb573397598260eb183fff9d985e8d3
Heon_9u <frtt0608@naver.com>
2021-06-24 16:58:09 +0900

    210624 | api 호출 클래스 분기

--- app/src/main/java/com/heon9u/alarm_weather_app/Openweather/FineDustApi.java
+++ app/src/main/java/com/heon9u/alarm_weather_app/Openweather/FineDustApi.java
@@ -1,14 +1,12 @@
 package com.heon9u.alarm_weather_app.Openweather;
 
+// 측정소별 실시간 측정정보 조회: getMsrstnAcctoRltmMesureDnsty
+
 public class FineDustApi {
-    // 기상청 미세먼지 api 연동 계획
 
-    private final String serviceKey = "Kv5twogKN1IrOKT7ufR7UtoIViGngsxpiWmwYxS1OxhdQvxzswoytOi3N169Ey2HpbuBafq3oqUtmzwYV8YZGA%3D%3D";
-    private final String msrstnUrl = "http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc";
+    private final String serviceKey = "iNSyWM3QGRc1%2B1ePlvp%2BAY0mZWRBAKGono6w8xfXe%2F%2Fs4pjupyF0wGgKWY6soqbZgerMYzi8l4r74LlsMU2i4A%3D%3D";
     private final String arpltnUrl = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc";
+    private final int MEASURE = 4;
 
-    // TM좌표 조회: getTMStdrCrdnt
-    // 근접 측정소 목록 조회: getNearbyMsrstnList
-    // 측정소별 실시간 측정정보 조회: getMsrstnAcctoRltmMesureDnsty
 
 }
